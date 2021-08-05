c6300f191502a441db025a791ebc24471259ff9f
cotymckinney <cotymckinney@outlook.com>
2021-08-04 22:21:52 -0400

    class inheritance end

--- index-4.php
+++ index-4.php
@@ -8,5 +8,29 @@
     Credit(s): Skeleton Demo File templated
 */
 
+    class First {
+        public $id = 23;
+        protected $name = "John Doe";
+
+        public function saySomething($word) {
+            echo $word;
+        }
+    }
+
+    class Second extends First {
+        public function getName() {
+            echo $this->name;
+        }
+    }
+
+    $second = new Second;
+
+    echo $second->getName();
+    
+    //echo $second->saySomething("Hello World!");
+
+
+
+
 
 ?>
