02248a2c77cc89e84c447102f16687843b7491d0
cotymckinney <cotymckinney@outlook.com>
2021-08-04 22:04:04 -0400

    Start class inheritance

---
+++ index-4.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Title: PHP OOP Practice
+    Description: Practicing with PHP OOP by building a simple site.
+    File Summary: Class inheritance
+    Date: 2021-08-04
+    Author: Coty McKinney
+    Credit(s): Skeleton Demo File templated
+*/
+
+
+?>
