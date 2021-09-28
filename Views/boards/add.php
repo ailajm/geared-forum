fafd2d16a75d3c02a69e626f9955f86f87920006
cotymckinney <cotymckinney@outlook.com>
2021-09-27 22:35:26 -0400

    Form framed in for board add

--- Views/boards/add.php
+++ Views/boards/add.php
@@ -11,4 +11,37 @@
 
 ?>
 
-    
+    <div class="form">
+        <form>
+            <div class="form-group">
+                <label>
+                    Share Title
+                </label>
+                <input 
+                    type="text"
+                    name="title"
+                    class="form-control" 
+                /> 
+            </div>
+            <div class="form-group">
+                <label>
+                    Body
+                </label>
+                <textarea name="body" class="form-control">
+                    Board description here....
+                </textarea> 
+            </div>
+            <div class="form-group">
+                <label>
+                    Posts
+                </label>
+                <input 
+                    type="text"
+                    name="posts"
+                    class="form-control" 
+                /> 
+            </div>
+            <input class="btn btn-success" name="submit" type="submit" value="submit"/>
+            <a class="btn btn-warning" href="<?php echo ROOT_URL;?>boards">Cancel</a>
+        </form>
+    </div>
