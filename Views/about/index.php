5c562399f7fe9d895387cf2ba1dffaff43f19cdc
cotymckinney <cmckin8741@gmail.com>
2021-10-12 16:01:14 -0400

    Implemented modals in about view for technologies.  Minor CSS tweaks.

--- Views/about/index.php
+++ Views/about/index.php
@@ -7,38 +7,76 @@
     Author: Coty McKinney
 */
 
-    echo "
-        <div class='aboutIndexPage'>
-            <h1>This is a simple php site built to practice OOP principles and PHP</h1>
-
-            <div class='technologyAndCredits'>
-                <h5><ins>Crafted using:</ins></h5>
-                <div class='skeletonCredits'>
-                    <h6>Skeleton</h6>
-                    <i class='fas fa-skull'></i>                    
-                    <a class='button button-primary' href='http://getskeleton.com'>View Skeleton Docs</a>
-                </div>
-                <div class='bootstrapCredits'>
-                    <h6>Bootstrap</h6>
-                    <i class='fab fa-bootstrap'></i>
-                    <a class='button button-primary' href='https://getbootstrap.com/docs/5.1/getting-started/introduction/'>Check out Bootstrap</a>
-                </div>
-                <div class='htmlCredits'>
-                    <h6>HTML</h6>
-                    <i class='fab fa-html5'></i>
-                    <a class='button button-primary' href='https://developer.mozilla.org/en-US/docs/Web/HTML'>Learn HTML</a>
-                </div>
-                <div class='cssCredits'>
-                    <h6>CSS</h6>
-                    <i class='fab fa-css3'></i>
-                    <a class='button button-primary' href='https://developer.mozilla.org/en-US/docs/Web/CSS'>Learn CSS</a>
-                </div>
-                <div class='phpCredits'>
-                    <h6>PHP</h6>
-                    <i class='fab fa-php'></i>
-                    <a class='button button-primary' href='https://www.php.net/docs.php'>Learn PHP</a>
-                </div>
-            </div>
-        </div>
-    ";
 ?>
+
+<div class='aboutIndexPage'>
+    <h1>This is a simple php site built to practice OOP principles and PHP</h1>
+
+    <h5><ins>Crafted using:</ins></h5>
+
+<div class='technologyAndCredits'>
+<div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <div class='phpCredits'>
+                <h6>PHP</h6>
+                <i class='fab fa-php'></i>
+                <a class='button button-primary' href='https://www.php.net/docs.php'>Learn PHP</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-2.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <div class='cssCredits'>
+                <h6>CSS</h6>
+                <i class='fab fa-css3'></i>
+                <a class='button button-primary' href='https://developer.mozilla.org/en-US/docs/Web/CSS'>Learn CSS</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <div class='htmlCredits'>
+                <h6>HTML</h6>
+                <i class='fab fa-html5'></i>
+                <a class='button button-primary' href='https://developer.mozilla.org/en-US/docs/Web/HTML'>Learn HTML</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <div class='bootstrapCredits'>
+                <h6>Bootstrap</h6>
+                <i class='fab fa-bootstrap'></i>
+                <a class='button button-primary' href='https://getbootstrap.com/docs/5.1/getting-started/introduction/'>Check out Bootstrap</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <div class='skeletonCredits'>
+                <h6>Skeleton</h6>
+                <i class='fas fa-skull'></i>                    
+                <a class='button button-primary' href='http://getskeleton.com'>View Skeleton Docs</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+</div>
